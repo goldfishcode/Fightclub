17be42dffc9cbf228c94efbb6c14baee519b51e8
goldfishcode <127666350+goldfishcode@users.noreply.github.com>
2023-03-26 03:45:14 +0200

    Create DecisionTree.cpp

---
+++ DecisionTree.cpp
@@ -0,0 +1,12 @@
+#include <cstdlib>
+#include "DecisionTree.h"
+#include "Character.h"
+
+
+DecisionTree::DecisionTree() {
+    // Constructor implementation
+}
+
+DecisionTree::~DecisionTree() {
+    // Destructor implementation
+}
